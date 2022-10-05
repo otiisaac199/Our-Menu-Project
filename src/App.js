889ca8cb3e1ba6eb1ef3111e0009e3f697a75cd8
otiisaac199 <otichinazaisaac19@gmail.com>
2022-10-05 22:58:20 +0100

    Features: added some useState Varible

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+
+function App() {
+  const [menuItem, setMenuItem] = useState(items);
+  const [categories, setCategories] = useState([]);
+
+  return <h2>menu project setup</h2>;
+}
+
+export default App;
