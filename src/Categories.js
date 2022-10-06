e092d65a8737ef28b89be7a83c5b7cd02988dc58
otiisaac199 <otichinazaisaac19@gmail.com>
2022-10-06 10:52:50 +0100

    features: passed down a props to the categories

--- src/Categories.js
+++ src/Categories.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Categories = ({ filterItems }) => {
+const Categories = ({ filterItems, categories }) => {
   return (
     <div className="btn-container">
       <button className="filter-btn" onClick={() => filterItems("all")}>
