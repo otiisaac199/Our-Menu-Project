891625ce52166ac6ed4e3258df1290d56081780f
otiisaac199 <otichinazaisaac19@gmail.com>
2022-10-06 10:56:37 +0100

    features: use the map function to get all buttons

--- src/Categories.js
+++ src/Categories.js
@@ -3,12 +3,18 @@ import React from "react";
 const Categories = ({ filterItems, categories }) => {
   return (
     <div className="btn-container">
-      <button className="filter-btn" onClick={() => filterItems("all")}>
-        all
-      </button>
-      <button className="filter-btn" onClick={() => filterItems("breakfast")}>
-        breakfast
-      </button>
+      {categories.map((category, index) => {
+        return (
+          <button
+            type="button"
+            className="filter-btn"
+            key={index}
+            onClick={() => filterItems(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
     </div>
   );
 };
